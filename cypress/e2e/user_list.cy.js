d466a165cff652fd3c6925eb2005d64244d4be45
JunHaoXia <99214774+JunHaoXia@users.noreply.github.com>
2023-11-06 11:19:11 -0500

    change back

--- cypress/e2e/user_list.cy.js
+++ cypress/e2e/user_list.cy.js
@@ -3,9 +3,7 @@ describe('user_list.cy.js', () => {
     cy.visit('https://blankmap-front-1626f242c2d7.herokuapp.com'); // Assuming your app is hosted at the root URL
 
     // Ensure the user list is displayed after loading
-    // cy.get('h2').should('contain', 'Available Users');
-    // wrong
-    cy.get('h2').should('contain', 'Available123');
+    cy.get('h2').should('contain', 'Available Users');
 
     // Verify that at least one user is listed
     cy.get('li').should('have.length.gt', 0);
