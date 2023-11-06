4f8ae27beb27fa11690f05ea7851748f09cb519e
Apricot0 <61392940+Apricot0@users.noreply.github.com>
2023-11-06 06:37:25 -0500

    cypress test

---
+++ cypress/e2e/user_list.cy.js
@@ -0,0 +1,11 @@
+describe('user_list.cy.js', () => {
+  it('should load and display the user list', () => {
+    cy.visit('https://blankmap-front-1626f242c2d7.herokuapp.com'); // Assuming your app is hosted at the root URL
+
+    // Ensure the user list is displayed after loading
+    cy.get('h2').should('contain', 'Available Users');
+
+    // Verify that at least one user is listed
+    cy.get('li').should('have.length.gt', 0);
+  });
+})
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,23 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+test('app renders', () => {
+  render(<App />);
+});
+
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+// test('renders a header', () => {
+//   const { getByText } = render(<App />);
+//   const headerElement = getByText(/Welcome to My Barebones Website/i);
+//   expect(headerElement).toBeInTheDocument();
+// });
+
+// test('renders a footer', () => {
+//   const { getByText } = render(<App />);
+//   const footerElement = getByText(/© 2023 My Barebones Website/i);
+//   expect(footerElement).toBeInTheDocument();
+// });
