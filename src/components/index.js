8dac6f2bc6b265f9945d3fc5a2f44fd180a72a3e
Sky Lei <ssskylei@gmail.com>
2023-11-12 16:18:56 -0500

    NAH

---
+++ src/components/index.js
@@ -0,0 +1,15 @@
+import AppBanner from './AppBanner'
+
+import LoginScreen from './LoginScreen'
+import RegisterScreen from './RegisterScreen'
+/*
+    This serves as a module so that we can import
+    all the other components as we wish.
+    
+    @author McKilla Gorilla
+*/
+export { 
+    AppBanner,  
+    LoginScreen,
+    RegisterScreen
+}
