01968f0efaaa3fa1600f1ca1662dd0f2f21274b6
Apricot0 <61392940+Apricot0@users.noreply.github.com>
2023-11-13 16:15:59 -0500

    test update

--- cypress/e2e/user_list.cy.js
+++ cypress/e2e/user_list.cy.js
@@ -1,11 +1,50 @@
-describe('user_list.cy.js', () => {
-  it('should load and display the user list', () => {
-    cy.visit('https://blankmap-front-1626f242c2d7.herokuapp.com'); // Assuming your app is hosted at the root URL
+describe('Normal Login', () => {
+  it('successfully loads', () => {
+    cy.visit('https://blankmap-front-1626f242c2d7.herokuapp.com'); // Assuming your app is served at http://localhost:3000/
 
-    // Ensure the user list is displayed after loading
-    cy.get('h2').should('contain', 'Available Users');
+    // Click the login button
+    cy.contains('Login').click();
 
-    // Verify that at least one user is listed
-    cy.get('li').should('have.length.gt', 0);
+    // Assert that the login was successful
+    cy.url().should('include', '/Home');
   });
-})
+});
+describe('Guest login', () => {
+  it('successfully loads', () => {
+    cy.visit('https://blankmap-front-1626f242c2d7.herokuapp.com'); // Assuming your app is served at http://localhost:3000/
+
+    // Click the guest login button
+    cy.contains('Guest Login').click();
+
+    // Assert that the guest login was successful
+    cy.url().should('include', '/Home');
+  });
+});
+
+describe('Register Screen', () => {
+  it('successfully loads', () => {
+    cy.visit('https://blankmap-front-1626f242c2d7.herokuapp.com'); // Assuming your app is served at http://localhost:3000/
+
+
+    // Click the "Register Account" link
+    cy.contains('Register Account').click();
+
+    // Assert that the URL includes '/register'
+    cy.url().should('include', '/register');
+
+  });
+});
+
+describe('Forgot Screen', () => {
+  it('successfully loads', () => {
+    cy.visit('https://blankmap-front-1626f242c2d7.herokuapp.com'); // Assuming your app is served at http://localhost:3000/
+
+
+    // Click the "Forgot Password" link
+    cy.contains('Forgot Password').click();
+
+    // Assert that the URL includes '/forgot'
+    cy.url().should('include', '/forgot');
+
+  });
+});
