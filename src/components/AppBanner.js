7e0cb845ba971f9d03428e0ba0aa999c3bddd762
Sky Lei <ssskylei@gmail.com>
2023-11-12 21:37:14 -0500

    Home screen edit

--- src/components/AppBanner.js
+++ src/components/AppBanner.js
@@ -3,12 +3,20 @@ import { Link } from 'react-router-dom'
 import AuthContext from '../auth';
 import { GlobalStoreContext } from '../store'
 import Box from '@mui/material/Box';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
 
 export default function AppBanner() {
 
     return(
-        <Box>
-            <h1>blankMap</h1>
+        <Box sx={{flexGrow: 1}}>
+            <AppBar position="static">
+                <Toolbar>
+                    
+                
+                </Toolbar>
+
+            </AppBar>
         </Box>
     );
 }
--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -2,6 +2,18 @@ import React, { useState } from "react";
 import MapOverview from "./MapOverview";
 import SearchBar from "./SearchBar";
 import { useNavigate } from "react-router-dom";
+import Box from '@mui/material/Box';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import { Button, Card, CardContent} from "@mui/material";
+import IconButton from "@mui/material/IconButton";
+import SearchIcon from "@mui/icons-material/Search";
+import TextField from "@mui/material/TextField";
 
 const HomeScreen = () => {
   // Assuming you have an array of map information
@@ -61,55 +73,105 @@ const HomeScreen = () => {
   const handlePageChange = (pageNumber) => {
     setCurrentPage(pageNumber);
   };
-  return (
-    <div className="home-screen">
-      {/* <h1>Home Screen</h1> */}
-      <SearchBar/>
-      <button onClick={handleCreateMap}>Create Map</button>
+  let searchBar=
+  <Box>
+      <AppBar position="sticky" sx={{ display: 'flex', alignItems: 'center' }}>
+      <form>
+      <TextField
+      id="search-bar"
+      className="text"
+      onInput={(e) => {
+      }}
+      label="What map are you looking for?"
+      variant="outlined"
+      placeholder="Search..."
+      size="medium"
+      sx={{ backgroundColor: 'white' }}
+    />
+    <IconButton type="submit" aria-label="search">
+      <SearchIcon style={{ fill: "white" }} />
+    </IconButton>
+    </form>
+      </AppBar>
+    </Box>
 
-      {/* Sort Options */}
-      <div className="sort-options">
-        <label>
-          Sort By:
-          <select onChange={(e) => handleSortChange(e.target.value)}>
-            <option value="">-- Select --</option>
-            <option value="title">Title</option>
-            {/* Add more sorting options as needed */}
-          </select>
-        </label>
-      </div>
+  let sorts=
+  <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+      <div sx={{ display: 'flex', gap: 2 }}>
+        <FormControl sx={{ minWidth: 120, marginRight: 2  }}>
+          <InputLabel id="sort-label">Sort By:</InputLabel>
+          <Select labelId="sort-label" id="sort-select" onChange={(e) => handleSortChange(e.target.value)}>
+            <MenuItem value="">
+              <em>-- Select --</em>
+            </MenuItem>
+            <MenuItem value="title">Title</MenuItem>
+          </Select>
+        </FormControl>
 
-      {/* Category Filter */}
-      <div className="filter-options">
-        <label>
-          Filter By Category:
-          <select onChange={(e) => handleCategoryFilter(e.target.value)}>
-            <option value="">-- Select --</option>
-            {/* Assume you have unique categories in your mapList */}
+        <FormControl sx={{ minWidth: 120 }}>
+          <InputLabel id="demo-simple-select-helper-label">Category:</InputLabel>
+          <Select
+            labelId="demo-simple-select-helper-label"
+            id="demo-simple-select-helper"
+            value={sortOption}
+            onChange={(e) => handleCategoryFilter(e.target.value)}
+          >
+            <MenuItem value="">
+              <em>-- Select --</em>
+            </MenuItem>
             {Array.from(new Set(mapList.flatMap((map) => map.tags))).map((category, index) => (
-              <option key={index} value={category}>
+              <MenuItem key={index} value={category}>
                 {category}
-              </option>
+              </MenuItem>
             ))}
-          </select>
-        </label>
-      </div>
-
-      <div className="map-overviews">
-        {currentMapList.map((mapInfo, index) => (
-          <MapOverview key={index} mapInfo={mapInfo} />
-        ))}
+          </Select>
+        </FormControl>
       </div>
 
       {/* Pagination Controls */}
-      <div className="pagination">
+      <div className="pagination" >
         {Array.from({ length: Math.ceil(filteredMapList.length / mapsPerPage) }, (_, index) => (
-          <button key={index} onClick={() => handlePageChange(index + 1)}>
+          <Button variant="outlined" key={index} onClick={() => handlePageChange(index + 1)}>
             {index + 1}
-          </button>
+          </Button>
         ))}
       </div>
+
+      {/* Typography Button on the right */}
+      <Typography>
+        <Button variant="contained" onClick={handleCreateMap}>Create Map</Button>
+      </Typography>
+    </Box> 
+
+const renderMapCards = () => {
+  return currentMapList.map((mapInfo, index) => (
+    <Card key={index} style={{ marginBottom: '20px' }}>
+      <CardContent>
+        <MapOverview mapInfo={mapInfo} />
+      </CardContent>
+      
+    </Card>
+  ));
+};
+
+  return (
+    <div>
+      {searchBar}
+      <div className="home-screen" style={{ paddingTop: '20px', paddingRight: '50px', paddingBottom: '30px', paddingLeft: '50px' }}>
+      {/* <h1>Home Screen</h1> */}
+      
+      {sorts}
+
+      
+        {/* {currentMapList.map((mapInfo, index) => (
+          <MapOverview key={index} mapInfo={mapInfo} />
+        ))} */}
+      {renderMapCards()}
+
+
     </div>
+    </div>
+    
   );
 };
 
