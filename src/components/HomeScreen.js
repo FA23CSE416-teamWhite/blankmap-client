e3bf239cce29807a49342645049037f8f0aec6e3
Apricot0 <61392940+Apricot0@users.noreply.github.com>
2023-11-20 02:40:17 -0500

    category drop down fix

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,21 +1,15 @@
 import React, { useState } from "react";
 import MapOverview from "./MapOverview";
-import SearchBar from "./SearchBar";
 import { useNavigate } from "react-router-dom";
 import Box from '@mui/material/Box';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import { Button, Card, CardContent } from "@mui/material";
-import IconButton from "@mui/material/IconButton";
-import SearchIcon from "@mui/icons-material/Search";
 import TextField from "@mui/material/TextField";
-import temp_map from './images/temp_map.png'
-
+import temp_map from './images/temp_map.png';
+import Autocomplete from '@mui/material/Autocomplete';
 const HomeScreen = () => {
   // Assuming you have an array of map information
   const navigate = useNavigate();
@@ -224,47 +218,42 @@ const HomeScreen = () => {
 
   let sorts =
     <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
-      <div sx={{ display: 'flex', gap: 2 }}>
-        <FormControl sx={{ minWidth: 120, marginRight: 2 }}>
-          <InputLabel id="sort-label">Sort By:</InputLabel>
-          <Select
-            labelId="sort-label"
-            id="sort-select"
-            onChange={(e) => handleSortChange(e.target.value)}
-            label="Sort By"
-            inputProps={{
-              style: { height: '36px' }, // Set the height here
-            }}
-          >
-            <MenuItem value="">
-              <em>-- Select --</em>
-            </MenuItem>
-            <MenuItem value="title">Title</MenuItem>
-          </Select>
-        </FormControl>
+<div style={{ display: 'flex', justifyContent: 'space-between', gap: '2px', alignItems: 'center' }}>
+  {/* Sort By */}
+  <FormControl style={{ minWidth: '120px', marginRight: '2px' }} size="small">
+    <InputLabel id="sort-label">Sort By:</InputLabel>
+    <Select
+      labelId="sort-label"
+      id="sort-select"
+      onChange={(e) => handleSortChange(e.target.value)}
+      label="Sort By"
+      inputProps={{
+        style: { height: '36px' },
+      }}
+    >
+      <MenuItem value="">
+        <em>-- Select --</em>
+      </MenuItem>
+      <MenuItem value="title">Title</MenuItem>
+    </Select>
+  </FormControl>
 
-        <FormControl sx={{ minWidth: 120 }}>
-          <InputLabel id="demo-simple-select-helper-label">Category:</InputLabel>
-          <Select
-            labelId="demo-simple-select-helper-label"
-            id="demo-simple-select-helper"
-            value={sortOption}
-            onChange={(e) => handleCategoryFilter(e.target.value)}
-            label="Category"
-          >
-            <MenuItem value="">
-              <em>-- Select --</em>
-            </MenuItem>
-            {Array.from(new Set(mapList.flatMap((map) => map.tags))).map((category, index) => (
-              <MenuItem key={index} value={category}>
-                {category}
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-
-
-      </div>
+  {/* Category */}
+  <Autocomplete
+    value={sortOption}
+    onChange={handleCategoryFilter}
+    options={Array.from(new Set(mapList.flatMap((map) => map.tags)))}
+    renderInput={(params) => (
+      <TextField
+        {...params}
+        label="Category"
+        size="small"
+        fullWidth
+      />
+    )}
+    style={{ minWidth: '120px', flex: 1 }}
+  />
+</div>
 
       {/* Pagination Controls */}
       <div className="pagination" >
@@ -273,7 +262,6 @@ const HomeScreen = () => {
             variant="outlined"
             key={index}
             onClick={() => handlePageChange(index + 1)}
-            sx={{ height: '50px' }} // Set the height here
           >
             {index + 1}
           </Button>
@@ -283,7 +271,7 @@ const HomeScreen = () => {
 
       {/* Typography Button on the right */}
       {/* <Typography> */}
-        <Button variant="contained" onClick={handleCreateMap}>Create Map</Button>
+      <Button variant="contained" onClick={handleCreateMap}>Create Map</Button>
       {/* </Typography> */}
     </Box>
 
