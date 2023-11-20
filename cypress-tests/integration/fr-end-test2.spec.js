839549003037475439a2b18a19e2d26eb3a8ec10
Sky Lei <ssskylei@gmail.com>
2023-11-20 00:02:33 -0500

    cypress

--- cypress-tests/integration/fr-end-test2.spec.js
+++
@@ -1,8 +0,0 @@
-//just a layout for tests, doesn't check yet since we have no buttons
-describe('Test2', () => {
-    it('Visits the app and clicks a button', () => {
-      cy.visit('/');
-      cy.get('button').click();
-      cy.contains('Button Clicked!');
-    });
-  });
