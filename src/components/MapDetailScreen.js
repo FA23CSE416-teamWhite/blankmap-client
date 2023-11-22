996c45406940079acf61a31a8868ae4c4e2e97c8
JunHaoXia <99214774+JunHaoXia@users.noreply.github.com>
2023-11-22 01:09:00 -0500

    did some like buttons and upvote comments, changed Tags.js

--- src/components/MapDetailScreen.js
+++ src/components/MapDetailScreen.js
@@ -1,23 +1,40 @@
 import React, { useState } from 'react';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import CommentIcon from '@mui/icons-material/Comment';
+import Tags from './Tags.js';
+import {
+    Box,
+    Typography,
+    Button,
+    TextField,
+    List,
+    ListItem,
+    IconButton,
+    Paper,
+    Grid,
+} from '@mui/material';
 import DownloadIcon from '@mui/icons-material/CloudDownload';
 import UpvoteIcon from '@mui/icons-material/ThumbUp';
 import DownvoteIcon from '@mui/icons-material/ThumbDown';
-import TextField from '@mui/material/TextField';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import IconButton from '@mui/material/IconButton';
-import Avatar from '@mui/material/Avatar';
-import Divider from '@mui/material/Divider';
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid';
 import temp_map from './images/temp_map.png'
 import { useNavigate } from 'react-router';
 
-const Comment = ({ comment, onReply, onUpvote, onDownvote, showReplyInput, replyText, setReplyText, handleAddReply }) => {
+const Comment = ({ comment, onReply, showReplyInput, replyText, setReplyText, handleAddReply }) => {
+    const [likes, setLikes] = useState(0);
+    const [dislikes, setDislikes] = useState(0);
+    const handleUpvote = () => {
+
+    };
+    const handleDownvote = () => {
+
+    };
+    const handleReply = () => {
+
+    };
+    const onUpvote = () => {
+        setLikes(likes + 1)
+    }
+    const onDownvote = () => {
+        setDislikes(dislikes + 1)
+    }
     return (
         <ListItem>
             <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', width: '100%' }}>
@@ -28,10 +45,10 @@ const Comment = ({ comment, onReply, onUpvote, onDownvote, showReplyInput, reply
                             Reply
                         </Button>
                         <Button onClick={onUpvote} color="primary" size="small">
-                            Upvote
+                            Upvote {likes}
                         </Button>
                         <Button onClick={onDownvote} color="primary" size="small">
-                            Downvote
+                            Downvote {dislikes}
                         </Button>
                     </div>
                     {showReplyInput && (
@@ -57,36 +74,73 @@ const Comment = ({ comment, onReply, onUpvote, onDownvote, showReplyInput, reply
 const MapDetailScreen = ({ mapDetails }) => {
     const { title, description, tags, mapImage, comments } = mapDetails;
     const [newComment, setNewComment] = useState('');
+    const [newTags, setNewTags] = useState('');
     const [commentList, setCommentList] = useState(comments);
+    const [newDescription, setDescription] = useState(description);
+    const [tagList, setTagList] = useState(tags);
+    const [likeColor, setLikeColor] = useState('grey');
+    const [dislikeColor, setDislikeColor] = useState('grey');
     const navigate = useNavigate();
 
     const handleAddComment = () => {
-        // Implement logic to add a comment (you may want to update your data structure)
-        console.log('Adding comment:', newComment);
-        // Clear the comment input
-        setNewComment('');
-        // Update the comment list
-        setCommentList([...commentList, newComment]);
+        if (newComment == ''){
+            console.log("Empty comment")
+        }
+        else {
+            // Implement logic to add a comment (you may want to update your data structure)
+            console.log('Adding comment:', newComment);
+            // Clear the comment input
+            setNewComment('');
+            // Update the comment list
+            setCommentList([...commentList, newComment]);
+        }
     };
+    const handleUpdateDescription = () => {
+        console.log('Updating description:', newDescription);
+    }
+    const handleAddTag = () => {
+        console.log('Adding tag:', newTags);
+        setNewTags('');
+    }
+    const handleDeleteTag = () => {
+        console.log('Deleting Tag:', newTags)
+        setNewTags('');
+    }
+    const handleLike = () => {
+        const newColor = likeColor === 'grey' ? 'steelblue' : 'grey';
+        setLikeColor(newColor);
+        if (dislikeColor === 'red') {
+            setDislikeColor('grey')
+        }
 
+    };
+    const handleDislike = () => {
+        const newColor = dislikeColor === 'grey' ? 'red' : 'grey';
+        setDislikeColor(newColor);
+        if (likeColor === 'steelblue') {
+            setLikeColor('grey')
+        }
+    };
     return (
         <Box sx={{ marginTop: 2, marginLeft: 2, marginRight: 2 }}>
             <Grid container spacing={2}>
                 <Grid item xs={12} md={4}>
                     <Paper elevation={3} sx={{ p: 2, borderRadius: 3, marginBottom: 2 }}>
                         <Typography variant="h6">Description</Typography>
-                        <Typography>{description}</Typography>
+                        <TextField
+                                type="description"
+                                placeholder="Add a description..."
+                                value={newDescription}
+                                onChange={(e) => setDescription(e.target.value)}
+                                multiline
+                                rows = {10}
+                                sx={{ height: '100%', width: '100%' }} 
+                            />
                     </Paper>
 
                     <Paper elevation={3} sx={{ p: 2, borderRadius: 3, marginBottom: 2 }}>
                         <Typography variant="h6">Tags</Typography>
-                        <div className="tags">
-                            {tags.map((tag, index) => (
-                                <span key={index} className="tag">
-                                    {tag}
-                                </span>
-                            ))}
-                        </div>
+                        <Tags tags={tags}></Tags>
                     </Paper>
 
                     <Box>
@@ -106,11 +160,11 @@ const MapDetailScreen = ({ mapDetails }) => {
                             <IconButton onClick={() => console.log('download button clicked')}>
                                 <DownloadIcon />
                             </IconButton>
-                            <IconButton onClick={() => console.log('upvote button clicked')}>
-                                <UpvoteIcon />
+                            <IconButton onClick={handleLike}>
+                                <UpvoteIcon style={{ color: likeColor }}/>
                             </IconButton>
-                            <IconButton onClick={() => console.log('downvote button clicked')}>
-                                <DownvoteIcon />
+                            <IconButton onClick={handleDislike}>
+                                <DownvoteIcon style={{ color: dislikeColor }}/>
                             </IconButton>
                         </Box>
                         <img src={temp_map} alt="Map" style={{ width: '100%', height: 'auto' }} />
