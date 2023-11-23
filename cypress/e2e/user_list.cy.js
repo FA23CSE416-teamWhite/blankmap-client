2934d6d0e805d279f3f7571c795eb8f91af07b92
Sky Lei <ssskylei@gmail.com>
2023-11-23 16:49:37 -0500

    Update user_list.cy.js

--- cypress/e2e/user_list.cy.js
+++ cypress/e2e/user_list.cy.js
@@ -1,50 +1,50 @@
-// describe('Normal Login', () => {
+// // describe('Normal Login', () => {
+// //   it('successfully loads', () => {
+// //     cy.visit('https://blankmap-front-1626f242c2d7.herokuapp.com/login'); // Assuming your app is served at http://localhost:3000/
+
+// //     // Click the login button
+// //     cy.contains('Login').click();
+
+// //     // Assert that the login was successful
+// //     cy.url().should('include', '/home');
+// //   });
+// // });
+// describe('Guest login', () => {
 //   it('successfully loads', () => {
 //     cy.visit('https://blankmap-front-1626f242c2d7.herokuapp.com/login'); // Assuming your app is served at http://localhost:3000/
 
-//     // Click the login button
-//     cy.contains('Login').click();
+//     // Click the guest login button
+//     cy.contains('Guest Login').click();
 
-//     // Assert that the login was successful
+//     // Assert that the guest login was successful
 //     cy.url().should('include', '/home');
 //   });
 // });
-describe('Guest login', () => {
-  it('successfully loads', () => {
-    cy.visit('https://blankmap-front-1626f242c2d7.herokuapp.com/login'); // Assuming your app is served at http://localhost:3000/
 
-    // Click the guest login button
-    cy.contains('Guest Login').click();
-
-    // Assert that the guest login was successful
-    cy.url().should('include', '/home');
-  });
-});
-
-describe('Register Screen', () => {
-  it('successfully loads', () => {
-    cy.visit('https://blankmap-front-1626f242c2d7.herokuapp.com/login'); // Assuming your app is served at http://localhost:3000/
+// describe('Register Screen', () => {
+//   it('successfully loads', () => {
+//     cy.visit('https://blankmap-front-1626f242c2d7.herokuapp.com/login'); // Assuming your app is served at http://localhost:3000/
 
 
-    // Click the "Register Account" link
-    cy.contains('Register Account').click();
+//     // Click the "Register Account" link
+//     cy.contains('Register Account').click();
 
-    // Assert that the URL includes '/register'
-    cy.url().should('include', '/register');
+//     // Assert that the URL includes '/register'
+//     cy.url().should('include', '/register');
 
-  });
-});
+//   });
+// });
 
-describe('Forgot Screen', () => {
-  it('successfully loads', () => {
-    cy.visit('https://blankmap-front-1626f242c2d7.herokuapp.com/login'); // Assuming your app is served at http://localhost:3000/
+// describe('Forgot Screen', () => {
+//   it('successfully loads', () => {
+//     cy.visit('https://blankmap-front-1626f242c2d7.herokuapp.com/login'); // Assuming your app is served at http://localhost:3000/
 
     
-    // Click the "Forgot Password" link
-    cy.contains('Forgot Password').click();
+//     // Click the "Forgot Password" link
+//     cy.contains('Forgot Password').click();
 
-    // Assert that the URL includes '/forgot'
-    cy.url().should('include', '/forgot');
+//     // Assert that the URL includes '/forgot'
+//     cy.url().should('include', '/forgot');
 
-  });
-});
+//   });
+// });
