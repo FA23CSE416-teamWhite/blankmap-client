f1c484d173821a528fca6ec690441672534fdb5d
Sky Lei <ssskylei@gmail.com>
2023-11-26 20:35:53 -0500

    store changes

--- src/store/index.js
+++ src/store/index.js
@@ -1,31 +1,37 @@
 import { createContext, useContext, useState } from 'react'
-import { useHistory } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import api from './store-request-api/index'
+import AuthContext from '../auth';
 
 
 export const GlobalStoreContext = createContext({});
-
+console.log("create GlobalStoreContext");
 export const GlobalStoreActionType = {
-    SET_QUERY_STRING: "SET_QUERY_STRING",
+    CREATE_NEW_MAP: "CREATE_NEW_MAP",
 }
 
 function GlobalStoreContextProvider(props) {
     const [globalStore, setGlobalStore] = useState({
-        queryString: ""
+        currentMap:null
     });
+    const navigate = useNavigate();
+    console.log("inside useGlobalStore");
+    const { auth } = useContext(AuthContext);
     const storeReducer = (action) => {
         const { type, payload } = action;
         switch (type) {
 
         }
     }
-    globalStore.createMap = function(title,description,publicStatus,tags,file){
-        async function asyncCreateMap(title,description,publicStatus,tags,file){
-            let response= await api.createMap(title,description,publicStatus,tags,file)
+    
+    globalStore.createMap = function(title,description,publicStatus,selectedCategory,tags,file){
+        console.log("hi")
+        async function asyncCreateMap(title,description,publicStatus,selectedCategory,tags,file){
+            let response= await api.createMap(title,description,publicStatus,selectedCategory,tags,file)
             if (response.data.success) {
-
+                console.log("good")
             }
-        }asyncCreateMap(title,description,publicStatus,tags,file)
+        }asyncCreateMap(title,description,publicStatus,selectedCategory,tags,file)
     }
     const setQueryString = (newQueryString) => {
         setGlobalStore({
@@ -33,12 +39,9 @@ function GlobalStoreContextProvider(props) {
             queryString: newQueryString
         })
     }
-    const contextValue = {
-        globalStore,
-        setQueryString
-    }
+    
     return (
-        <GlobalStoreContext.Provider value={{contextValue}}>
+        <GlobalStoreContext.Provider value={{globalStore}}>
             {props.children}
         </GlobalStoreContext.Provider>
     );
--- src/store/store-request-api/index.js
+++ src/store/store-request-api/index.js
@@ -1,15 +1,16 @@
 import axios from 'axios'
 axios.defaults.withCredentials = true;
 const api = axios.create({
-    baseURL: 'https://blankmap-server-6de6d45e4291.herokuapp.com/auth'
+    baseURL: 'https://blankmap-server-6de6d45e4291.herokuapp.com/api'
 })
 
 // export const createMap = () => api.post(`/map/`);
-export const createMap = (title,description,publicStatus,tags,file) => {
-    return api.post(`/map/`, {
+export const createMap = (title,description,publicStatus,selectedCategory,tags,file) => {
+    return api.post(`/map/createMap`, {
         title: title,
         description: description,
         publicStatus:publicStatus,
+        selectedCategory:selectedCategory,
         tags: tags,
         file: file
     })
