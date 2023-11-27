eb86c317675bfce77f25b27fa19c6c3c146fc9d1
JunHaoXia <99214774+JunHaoXia@users.noreply.github.com>
2023-11-26 20:09:10 -0500

    just made feature boxes for now

--- src/components/MapEdit.js
+++ src/components/MapEdit.js
@@ -1,6 +1,8 @@
-import React, { useState, useEffect, Component } from "react";
+import React, { useState, useEffect, Component, useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { GlobalStoreContext } from '../store/index'; 
 import Grid from "@mui/material/Grid";
+import IconButton from '@mui/material/IconButton';
 import {
     Box,
     Typography,
@@ -23,29 +25,50 @@ import Card from "@mui/material/Card";
 import SquareIcon from '@mui/icons-material/Square';
 
 const MapEdit = () => {
-    const [features, setFeatures] = useState([]);
-
+    const tempMapData = {
+        addedFeatures: [
+        { type: "String", name: "Name" },
+        { type: "Number", name: "Population" },
+        { type: "Number", name: "Area" },
+        { type: "Number", name: "GDP" },
+        { type: "Number", name: "HDI" },],
+        baseData: [],
+        mapType: "Choropleth"
+    }
+    const [features, setFeatures] = useState(tempMapData.addedFeatures);
+    const [newFeature, setNewFeature] = useState("");
+    const { globalStore, setQueryString } = useContext(GlobalStoreContext);
     const [selectedFeatureType, setSelectedFeatureType] = useState("");
-    const [featureName, setFeatureName] = useState("");
-    useEffect(() => {
-        setFeatures([
-            { type: "String", name: "Name" },
-            { type: "Number", name: "Population" },
-            { type: "Number", name: "Area" },
-            { type: "Number", name: "GDP" },
-            { type: "Number", name: "HDI" },
-        ]);
-    }, []);
-
+    const [pickColor, setPickColor] = useState("");
+    let displayFeatures = features.map((feature, index) => (
+        <IconButton
+          key={index}
+          sx={{
+            fontSize: '10px',
+            backgroundColor: '#0844A4',
+            color: 'white',
+            padding: '5px',
+            borderRadius: '10px',
+            margin: '0 4px',
+            boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.2)',
+            transition: 'background-color 0.3s', // Add transition for smooth color change
+            ':hover': {
+              backgroundColor: '#0A5CE8', // Change color on hover
+            },
+          }}
+        >
+          {feature.name}
+        </IconButton>
+      ));
     const handleAddFeature = () => {
-        if (selectedFeatureType && featureName) {
+        if (selectedFeatureType && newFeature) {
             setFeatures([
                 ...features,
-                { type: selectedFeatureType, name: featureName },
+                { type: selectedFeatureType, name: newFeature },
             ]);
 
             setSelectedFeatureType("");
-            setFeatureName("");
+            setNewFeature("");
         }
     };
 
@@ -62,7 +85,10 @@ const MapEdit = () => {
     const handleRankFeatures = () => {
         console.log("Ranking features...");
     };
-
+    const handleUndo = () => {
+    }
+    const handleRedo = () => {
+    }
     return (
         <Grid container>
             <Grid item xs={12} sm={1}></Grid>
@@ -107,8 +133,8 @@ const MapEdit = () => {
                     </Grid>
                     <Grid item xs={12} sm={3}>
                         <Box>
-                            <UndoIcon sx={{ mr: 1 }} />
-                            <Redo />
+                            <UndoIcon sx={{ mr: 1 }} onClick={handleUndo}/>
+                            <Redo onClick={handleRedo}/>
                         </Box>
                     </Grid>
                 </Grid>
@@ -116,7 +142,7 @@ const MapEdit = () => {
                 <Box sx={{ paddingY: 2 }} />
                 <Box>
                     <Typography>
-                        Features: Name, Population
+                        Features: {displayFeatures}
                     </Typography>
                     <Link>edit</Link>
                 </Box>
@@ -126,23 +152,31 @@ const MapEdit = () => {
                 <Card>
                     <CardContent>
                         <Typography>Feature Name:</Typography>
-                        <TextField fullWidth label="Population"></TextField>
+                        <TextField
+                                type="text"
+                                fullWidth
+                                placeholder="Add a feature..."
+                                value={newFeature}
+                                onChange={(e) => setNewFeature(e.target.value)}
+                                size="small"
+                                sx={{ height: '40px' }} 
+                            />
                     </CardContent>
                 </Card>
-                <Button variant="contained" sx={{ paddingY: 1 }}> Add More Features</Button>
+                <Button variant="contained" sx={{ paddingY: 1 }} onClick={handleAddFeature}> Add More Features</Button>
                 <Box sx={{ paddingY: 2 }} />
 
                 <Typography>
                     Feature for Choropleth
                 </Typography>
                 <TextField label="Choose a Feature"></TextField>
-                <Typography> Choose a Color</Typography>
+                <Typography> Choose a Color: {pickColor}</Typography>
                 <Box sx={{ paddingY: 1 }}>
-                    <SquareIcon sx={{ color: "red", paddingX: 1 }} />
-                    <SquareIcon sx={{ color: "blue", paddingX: 1 }} />
-                    <SquareIcon sx={{ color: "yellow", paddingX: 1 }} />
-                    <SquareIcon sx={{ color: "green", paddingX: 1 }} />
-                    <SquareIcon sx={{ color: "purple", paddingX: 1 }} />
+                    <SquareIcon sx={{ color: "red", paddingX: 1 }} onClick={() => setPickColor("red")}/>
+                    <SquareIcon sx={{ color: "blue", paddingX: 1 }} onClick={() => setPickColor("blue")}/>
+                    <SquareIcon sx={{ color: "yellow", paddingX: 1 }} onClick={() => setPickColor("yellow")}/>
+                    <SquareIcon sx={{ color: "green", paddingX: 1 }} onClick={() => setPickColor("green")}/>
+                    <SquareIcon sx={{ color: "purple", paddingX: 1 }} onClick={() => setPickColor("purple")}/>
                 </Box>
 
                 <Box>
