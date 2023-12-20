d0709224b6746577227aeb07579472ee2cb67cc0
Apricot0 <61392940+Apricot0@users.noreply.github.com>
2023-12-20 11:28:03 -0500

    cypress update

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,18 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
+  "defaultCommandTimeout": 10000,  // Set the timeout to 10 seconds (in milliseconds)
+  
+  "e2e": {
+    "setupNodeEvents": (on, config) => {
       // implement node event listeners here
-    },
+    }
   },
-
-  component: {
-    devServer: {
-      framework: "create-react-app",
-      bundler: "webpack",
-    },
-  },
+  
+  "component": {
+    "devServer": {
+      "framework": "create-react-app",
+      "bundler": "webpack"
+    }
+  }
 });
